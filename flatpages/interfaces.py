49130581d641cfddd53e9ad6b32d7fcbd4220f59
Brandon R. Stoner <monokrome@monokro.me>
2011-03-30 02:05:49 -0600

    Added an interface for hanging flatpages off of.

---
+++ flatpages/interfaces.py
@@ -0,0 +1,9 @@
+from zope.interface import Interface
+
+class IFlatPagesRoot(Interface):
+    """ Any class that implements this interface will be respected as the root
+    of our flatpages URI.
+
+    """
+
+    pass
